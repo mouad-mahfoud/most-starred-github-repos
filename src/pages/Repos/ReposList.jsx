fe80ff385bef406d8ed814ee1c7703b5741fed7d
Mouad <devmahfoud@gmail.com>
2020-04-23 00:52:00 +0000

    installation and implementation of  Infinite Scroll

--- src/pages/Repos/ReposList.jsx
+++ src/pages/Repos/ReposList.jsx
@@ -4,6 +4,7 @@ import List from "@material-ui/core/List";
 import { Container } from "@material-ui/core";
 import { getRepos } from "../../API";
 import RepoItem from "../../components/Repo/RepoItem";
+import InfiniteScroll from "react-infinite-scroller";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -15,27 +16,49 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+let page = 1;
+
 export default function ReposList() {
   const classes = useStyles();
-  const [repos, setRepos] = useState();
+  const [repos, setRepos] = useState([]);
+  const [incompleteResults, setIncompleteResults] = useState();
+  const [initialized, setInitialized] = useState(false);
 
   useEffect(() => {
-    (async () => {
-      const response = await getRepos();
-      setRepos(response.items);
-    })();
+    if (!initialized) {
+      fetchMoreListItems();
+    }
   });
+
+  const fetchMoreListItems = async () => {
+    const response = await getRepos(page);
+    setRepos(repos.concat(response.items));
+    setIncompleteResults(response.incomplete_results);
+    page++;
+    setInitialized(true);
+  };
 
   return (
     <Container>
       {repos ? (
         <List className={classes.root}>
-          {repos.map((item, index) => (
-            <RepoItem repo={item} key={index} />
-          ))}
+          <InfiniteScroll
+            pageStart={page}
+            loadMore={fetchMoreListItems}
+            hasMore={incompleteResults}
+            loader={
+              <div className="loader" key={0}>
+                fetching more repos ...
+              </div>
+            }
+          >
+            {repos.map((item, index) => (
+              <RepoItem repo={item} key={index} />
+            ))}
+          </InfiniteScroll>
         </List>
       ) : (
-        <h2>Loading......</h2>
+        <h2>Loading .....</h2>
       )}
     </Container>
   );
